85dd7d9996c5868233091b909a9a1b8064f59f44
Dheeraj-cloud <71017329+Dheeraj-cloud@users.noreply.github.com>
2022-08-31 18:48:36 +0530

    Updates

---
+++ precache-manifest.c1ab519cab1718f79d740b8f824c41a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060c07b7733eef82c441803f62b58c4b",
+    "url": "/Dheeraj-cloud/Movies_list/index.html"
+  },
+  {
+    "revision": "90ed070a51992c0751cd",
+    "url": "/Dheeraj-cloud/Movies_list/static/css/main.e789e801.chunk.css"
+  },
+  {
+    "revision": "fe846442e05d43de3377",
+    "url": "/Dheeraj-cloud/Movies_list/static/js/2.3acb1463.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/Dheeraj-cloud/Movies_list/static/js/2.3acb1463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ed070a51992c0751cd",
+    "url": "/Dheeraj-cloud/Movies_list/static/js/main.bcb62326.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Dheeraj-cloud/Movies_list/static/js/main.bcb62326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380a504064e2a426cbb0",
+    "url": "/Dheeraj-cloud/Movies_list/static/js/runtime-main.b9797eb3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7957dd793d26f2904c31eaf405783329.js"
+  "/Dheeraj-cloud/Movies_list/precache-manifest.c1ab519cab1718f79d740b8f824c41a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dheeraj-cloud/Movies_list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
