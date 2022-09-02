3bde37035a969aa06aaed5c43b88f8f16aeb0793
Dheeraj-cloud <71017329+Dheeraj-cloud@users.noreply.github.com>
2022-09-02 20:33:41 +0530

    Updates

---
+++ precache-manifest.7570e3724ad5ba1422ea911f27038791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd86008dadff40c00454eaaf9f030015",
+    "url": "./index.html"
+  },
+  {
+    "revision": "231401be347c0118afa9",
+    "url": "./static/css/main.e789e801.chunk.css"
+  },
+  {
+    "revision": "fe846442e05d43de3377",
+    "url": "./static/js/2.3acb1463.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "./static/js/2.3acb1463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231401be347c0118afa9",
+    "url": "./static/js/main.e22d8b54.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.e22d8b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a768fd8a90a9409c55e",
+    "url": "./static/js/runtime-main.ee9b8a1b.js"
+  }
+]);
